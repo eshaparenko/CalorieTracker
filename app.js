a4a416d6cd29eebe2b10558657412e25afa588f5
eshaparenko <gesha8610@gmail.com>
2023-03-31 20:11:15 +0300

    Added populate list logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Storage Controller
 
 //Item Controller
-//IIFE
-const ItemCtrl = (()=>{
+//IIFE(Molule pattern)
+const ItemCtrl = (() => {
     //Item Constructor
     const Item = (id, name, calories)=>{
         this.id = id;
@@ -15,7 +15,9 @@ const ItemCtrl = (()=>{
         items: [
             {id: 0, name: 'Staek Dinner', calories: 1200},
             {id: 1, name: 'Cookie', calories: 300},
-            {id: 2, name: 'Eggs', calories: 300}
+            {id: 2, name: 'Eggs', calories: 300},
+            {id: 3, name: 'Apple', calories: 30}
+
 
         ],
         currentItem: null,
@@ -24,6 +26,7 @@ const ItemCtrl = (()=>{
 
     //Public methods
     return {
+        getItems: () => data.items,
         logData: () => data,
         totalCalories: 0
     }
@@ -31,18 +34,43 @@ const ItemCtrl = (()=>{
 
 //UI Controller
 const UiCtrl = (()=>{
+    const UISelectors = {
+        itemList: '#item-list'
+    }
+
     //Public methods
     return {
+        populateItemList: items => {
+            let html = '';
+            items.forEach(item => {
+                html += `
+                    <li class="collection-item" id="item-0">
+                    <strong>${item.name} </strong><em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="fa fa-pencil"></i>
+                    </a>
+                </li>`;
 
+                const itemListElement = document.querySelector(UISelectors.itemList);
+                itemListElement.innerHTML = html;
+            });
+        }
     }
 })()
 
-
 //App Controller
-const App = ((item, ui)=>{
+const App = (()=>{
     //Public methods
     return {
-        init: () => console.log(item.logData())
+        init: () => {
+            //Fetch Items
+            const items = ItemCtrl.getItems()
+
+            //Populate list with Items
+
+            UiCtrl.populateItemList(items);
+            
+        }
     }
 })(ItemCtrl, UiCtrl)
 
